c4ce6d88d6904f18218b9befc8a3f4c520c812cb
CARMEN QUESADA <carmenquesadarguez@gmail.com>
2025-07-09 17:24:35 +0200

    Si usuario logueado, devolver sus restaurantes

--- DeliverUS-Frontend-Owner/src/screens/restaurants/RestaurantsScreen.js
+++ DeliverUS-Frontend-Owner/src/screens/restaurants/RestaurantsScreen.js
@@ -1,5 +1,5 @@
 /* eslint-disable react/prop-types */
-import React, { useEffect, useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { StyleSheet, View, FlatList, Pressable } from 'react-native'
 import TextRegular from '../../components/TextRegular'
 import { getAll } from '../../api/RestaurantEndpoints'
@@ -8,18 +8,38 @@ import ImageCard from '../../components/ImageCard'
 import TextSemiBold from '../../components/TextSemibold'
 import { API_BASE_URL } from '@env'
 import restaurantLogo from '../../../assets/restaurantLogo.jpeg'
+import { showMessage } from 'react-native-flash-message'
 
 export default function RestaurantsScreen({ navigation }) {
   const [restaurants, setRestaurants] = useState([])
+  // Usaremos un useContext que guardará la info de quién está logueado en un lugar accesible para todas las pantallas
+  // 1. Obtener el usuario logueado desde el contexto: trae del AuthorizationContext el usuario que está logueado
+  // loggedInUser: objeto con los datos del usuario logueado
+  // AuthorizationContext: guarda si hay un usuario logueado o no y, si lo hay, guarda sus datos
+  // 2. Si nadie ha iniciado sesión: no hacemos petición
+  // 2. Si hay alguien logueado: hacemos petición para pedir sus restaurantes
 
+  const { loggedInUser } = useContext(AuthorizationContext)
   useEffect(() => {
-    console.log('Loading restaurants, please wait 2 seconds')
-    setTimeout(() => {
-      setRestaurants(getAll) // getAll function has to be imported
-      console.log('Restaurants loaded')
-    }, 2000)
-  }, [])
-
+    async function fetchRestaurants () { 
+      try {
+        const fetchedRestaurants = await getAll() // Llama a la función getAll() y guarda la respuesta en la variable fetchedRestaurants
+        setRestaurants(fetchedRestaurants) // Actualiza el estado con los nuevos datos
+      } catch (error) { // Error handling
+        showMessage({
+          message: `There was an error while retrieving restaurants. ${error} `,
+          type: 'error',
+          style: GlobalStyles.flashStyle,
+          titleStyle: GlobalStyles.flashTextStyle
+        })
+      }
+    }
+    if (loggedInUser) { // Only fetched if a loggedInUser exists
+      fetchRestaurants()
+    } else {
+      setRestaurants(null)
+    }
+  }, [loggedInUser]) // Ejecutar cuando cambia loggedInUser
   const renderRestaurant = ({ item }) => {
     return (
       <Pressable
